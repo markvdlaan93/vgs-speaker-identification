9457de3f8bcf9f3f581849c0916eabfe5521780b
Mark van der Laan <markkvdlaan93@gmail.com>
2018-04-10 23:03:22 +0200

    Method for classifying mfcc vectors

--- classify.py
+++ classify.py
@@ -1,7 +1,18 @@
 import preprocessing
 import load_data
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, f1_score
+import numpy as np
 
 val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc = load_data.dataset()
 
 def mfcc():
-    return None
+    X_train, X_val, y_train, y_val = train_test_split(val_mfcc, val_spk, test_size=0.2, random_state=123)
+
+    model = SGDClassifier(loss='log', random_state=123, max_iter=1000)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_val)
+    return f1_score(y_val, y_pred, average='weighted', labels=np.unique(y_pred))
+
+print(mfcc())
--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,6 @@ from sklearn.preprocessing import OneHotEncoder
 
 def one_hot_encoding(y_train, y_val):
     encoder = OneHotEncoder(sparse=False)
-    y_train = encoder.fit_transform(y_train)
-    y_val = encoder.transform(y_val)
+    y_train = encoder.fit_transform(y_train.reshape(-1, 1))
+    y_val = encoder.transform(y_val.reshape(-1, 1))
     return y_train, y_val
