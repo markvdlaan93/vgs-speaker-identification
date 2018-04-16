1d070713d969bc46411dc6cde29145515d54d957
Mark van der Laan <markkvdlaan93@gmail.com>
2018-04-16 15:22:55 +0200

    Shift no tuned code into separated method

--- classify.py
+++ classify.py
@@ -7,7 +7,7 @@ import numpy as np
 NUM_TRIALS = 30
 val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc = load_data.dataset()
 
-def mfcc():
+def mfcc_no_tuning():
     """
     First run without further hyperparameter tuning (i.e. loss='log', max_iter=1000, n_jobs=1, learning_rate='optimal')
     Score for fold 1 is 0.8106473829790354
@@ -17,6 +17,27 @@ def mfcc():
     Score for fold 5 is 0.8026816159176354
     :return:
     """
+    X_train, X_test, y_train, y_test = train_test_split(val_mfcc, val_spk, test_size=0.2, random_state=123)
+
+    kf = KFold(n_splits=5, random_state=123)
+    result = {}
+    count = 1
+
+    for train_index, test_index in kf.split(X_train):
+        fold_x_train, fold_x_test = X_train[train_index], X_train[test_index]
+        fold_y_train, fold_y_test = y_train[train_index], y_train[test_index]
+
+        model = SGDClassifier(loss='log', random_state=123, max_iter=1000, n_jobs=-1)
+        model.fit(fold_x_train, fold_y_train)
+        y_pred = model.predict(fold_x_test)
+        score = f1_score(fold_y_test, y_pred, average='weighted', labels=np.unique(y_pred))
+        print("Score for fold {} is {}".format(count, score))
+        result[count] = score
+
+    return result
+
+def mfcc_cv_tuning():
+
     X_train, X_test, y_train, y_test = train_test_split(val_mfcc, val_spk, test_size=0.2, random_state=123)
 
     p_grid = {
@@ -59,5 +80,5 @@ def conv():
     y_pred = model.predict(X_val)
     return f1_score(y_val, y_pred, average='weighted', labels=np.unique(y_pred))
 
-print(mfcc())
+print(mfcc_cv_tuning())
 # print(conv())
