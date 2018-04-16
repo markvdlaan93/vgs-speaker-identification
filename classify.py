2bfc7bae920725a89b1de94a613e342decb2c5f3
Mark van der Laan <markkvdlaan93@gmail.com>
2018-04-16 15:09:15 +0200

    Add nested cross validation for optimal tuning

--- classify.py
+++ classify.py
@@ -1,11 +1,10 @@
 import load_data
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, make_scorer
 import numpy as np
-import pprint
 
+NUM_TRIALS = 30
 val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc = load_data.dataset()
 
 def mfcc():
@@ -20,24 +19,36 @@ def mfcc():
     """
     X_train, X_test, y_train, y_test = train_test_split(val_mfcc, val_spk, test_size=0.2, random_state=123)
 
-    kf = KFold(n_splits=5, random_state=123)
-    result = {}
-    count = 1
-    
-    for train_index, test_index in kf.split(X_train):
-        fold_x_train, fold_x_test = X_train[train_index], X_train[test_index]
-        fold_y_train, fold_y_test = y_train[train_index], y_train[test_index]
+    p_grid = {
+        'loss': ['hinge', 'log', 'perceptron'],
+        'learning_rate': ['optimal', 'constant', 'invscaling']
+    }
+    sgd = SGDClassifier(max_iter=1000, n_jobs=-1)
+    non_nested_scores = np.zeros(NUM_TRIALS)
+    nested_scores = np.zeros(NUM_TRIALS)
 
-        model = SGDClassifier(loss='log', random_state=123, max_iter=1000, n_jobs=-1)
-        model.fit(fold_x_train, fold_y_train)
-        y_pred = model.predict(fold_x_test)
-        score = f1_score(fold_y_test, y_pred, average='weighted', labels=np.unique(y_pred))
-        print("Score for fold {} is {}".format(count, score))
-        result[count] = score
+    for i in range(NUM_TRIALS):
+        print("BEGIN TRIAL {}".format(NUM_TRIALS))
 
-        count += 1
+        inner_fold = KFold(n_splits=5, random_state=i, shuffle=True)
+        outer_fold = KFold(n_splits=5, random_state=i, shuffle=True)
 
-    return result
+        scorer = make_scorer(f1_score, average='weighted')
+        classifier = GridSearchCV(estimator=sgd, param_grid=p_grid, cv=inner_fold, scoring=scorer)
+        classifier.fit(X_train, y_train)
+        non_nested_scores[i] = classifier.best_score_
+
+        print("BEST SCORE FOR NON NESTED FOLD: {}".format(classifier.best_score_))
+        print("BEST PARAMS FOR NON NESTED FOLD: {}".format(classifier.best_params_))
+
+        nested_score = cross_val_score(classifier, X=X_train, y=y_train, cv=outer_fold, scoring=scorer)
+        nested_scores[i] = nested_score.mean()
+
+        print("BEST SCORE FOR NESTED FOLD: {}".format(nested_score.mean()))
+
+        print("END TRAIL {}".format(NUM_TRIALS))
+
+    return non_nested_scores, nested_scores
 
 
 def conv():
