11ee99458cd01fd718676e709695b9e3711b618d
Mark van der Laan <markkvdlaan93@gmail.com>
2018-04-20 12:41:57 +0200

    Methods for plotting results

--- explore.py
+++ explore.py
@@ -38,6 +38,29 @@ def hypothesis():
     plt.ylabel('Error rate')
     plt.show()
 
+def results_no_tuning_f1_score():
+    avg_f1 = np.array([
+        0.80033, 0.80062, 0.9472302716385617, 0.9165205223061058, 0.8791941433923919, 0.8460119386623735, 0.5614487580601043
+    ])
+
+    x = [-2, -1, 0, 1, 2, 3, 4]
+    plt.bar(x,avg_f1,align='center')
+    plt.xlabel('Learning directly from MFCC (-2), Convolutional layer(-1), Recurrent layer (0 till 4)')
+    plt.ylabel('F1-score')
+    plt.savefig('./img/result_no_tuning_f1_score.png')
+
+def results_no_tuning_acc():
+    x = [-2, -1, 0, 1, 2, 3, 4]
+    avg_acc = np.array([0.75025, 0.75775, 0.9295, 0.89375, 0.85275, 0.82075, 0.5175])
+
+    plt.bar(x, avg_acc, align='center')
+    plt.xlabel('Learning directly from MFCC (-2), Convolutional layer(-1), Recurrent layer (0 till 4)')
+    plt.ylabel('Accuracy')
+    plt.savefig('./img/result_no_tuning_accuracy.png')
+
+results_no_tuning_f1_score()
+results_no_tuning_acc()
+
 # shapes([val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc])
 # counts, majority = majority()
 # speaker_occurrences(counts)
--- preprocessing.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import OneHotEncoder
-
-def one_hot_encoding(y_train, y_val):
-    encoder = OneHotEncoder(sparse=False)
-    y_train = encoder.fit_transform(y_train.reshape(-1, 1))
-    y_val = encoder.transform(y_val.reshape(-1, 1))
-    return y_train, y_val
