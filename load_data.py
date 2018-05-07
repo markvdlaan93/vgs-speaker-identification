275d592e2a9fd9ad20e37af5e6cf4d5bf2f4d7cc
Mark van der Laan <markkvdlaan93@gmail.com>
2018-05-07 19:55:30 +0200

    Add __init__ module files and load data

--- load_data.py
+++ load_data.py
@@ -1,38 +1,39 @@
 import numpy as np
 
+ABS_PATH = '/Applications/MAMP/htdocs/master-thesis/'
 
 def dataset():
     # Values of the activation functions (64 values per speech signal) => (5000,64)
-    val_conv = np.load('data/flickr8k_val_conv.npy')
+    val_conv = np.load(ABS_PATH + 'data/flickr8k_val_conv.npy')
     # Output layer (5000,1024)
-    val_emb = np.load('data/flickr8k_val_emb.npy')
+    val_emb = np.load(ABS_PATH + 'data/flickr8k_val_emb.npy')
     # For each recurrent layer, the values of the activation functions (1024 values per layer) for each speech signal
     # => (5000, 4, 1024)
-    val_rec = np.load('data/flickr8k_val_rec.npy')
+    val_rec = np.load(ABS_PATH + 'data/flickr8k_val_rec.npy')
     # Per speech signal, the ID of the speaker => (5000,)
-    val_spk = np.load('data/flickr8k_val_spk.npy')
+    val_spk = np.load(ABS_PATH + 'data/flickr8k_val_spk.npy')
     # Caption of each speech signal => (5000,)
-    val_text = np.load('data/flickr8k_val_text.npy')
+    val_text = np.load(ABS_PATH + 'data/flickr8k_val_text.npy')
     # MFCC vector per speech signal with 37 coefficients => (5000,37)
-    val_mfcc = np.load('data/flickr8k_val_mfcc.npy')
+    val_mfcc = np.load(ABS_PATH + 'data/flickr8k_val_mfcc.npy')
 
     return val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc
 
 
 def dataset_places():
     # Values of the activation functions (64 values per speech signal) => (1000,64)
-    val_conv = np.load('data/places_val_conv.npy')
+    val_conv = np.load(ABS_PATH + 'data/places_val_conv.npy')
     # Output layer (1000,1024)
-    val_emb = np.load('data/places_val_emb.npy')
+    val_emb = np.load(ABS_PATH + 'data/places_val_emb.npy')
     # For each recurrent layer, the values of the activation functions (1024 values per layer) for each speech signal
     # => (1000, 4, 1024)
-    val_rec = np.load('data/places_val_rec.npy')
+    val_rec = np.load(ABS_PATH + 'data/places_val_rec.npy')
     # Per speech signal, the ID of the speaker => (1000,)
-    val_spk = np.load('data/places_val_spk.npy')
+    val_spk = np.load(ABS_PATH + 'data/places_val_spk.npy')
     # Caption of each speech signal => (1000,)
-    val_text = np.load('data/places_val_text.npy')
+    val_text = np.load(ABS_PATH + 'data/places_val_text.npy')
     # MFCC vector per speech signal with 37 coefficients => (1000,13) @todo normalize
-    val_mfcc = np.load('data/places_val_mfcc.npy')
+    val_mfcc = np.load(ABS_PATH + 'data/places_val_mfcc.npy')
 
     return val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc
 
--- places/explore_places.py
+++ places/explore_places.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+sys.path.append('../')
+
 import load_data
+import majority
 
 val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc = load_data.dataset_places()
 
+def distribution():
+    print(majority.majority(val_spk))
+
+distribution()
