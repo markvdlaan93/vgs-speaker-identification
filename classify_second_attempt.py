b9eaf75fa07c23851e2c0d2be46e7f73d9d4c638
Mark van der Laan <markkvdlaan93@gmail.com>
2018-06-27 19:53:12 +0200

    Split dataset according to 60/20/20 rule

--- classify_second_attempt.py
+++ classify_second_attempt.py
@@ -3,6 +3,29 @@ from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_sc
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score, f1_score, make_scorer
 import numpy as np
+import pandas as pd
+from pandas import DataFrame
 
 val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc = load_data.dataset()
 N_SPLITS = 5
+
+def split_set(data_set):
+    """
+    Generic method for splitting a dataset into train, validation and test set.
+    :param data_set:
+    :return:
+    """
+
+    df = DataFrame(val_mfcc)
+    train_set, validation_set, test_set = np.split(df.sample(frac=1), [int(.6 * len(df)), int(.8 * len(df))])
+    return train_set, validation_set, test_set
+
+def mfcc():
+    """
+    Classification on the MFCC layer
+    :return:
+    """
+    train_set, validation_set, test_set = split_set()
+
+
+mfcc()
