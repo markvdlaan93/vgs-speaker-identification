cd5340d6394da9c016b9d465fa636265ba9eaf02
Mark van der Laan <markkvdlaan93@gmail.com>
2018-06-28 02:35:08 +0200

    Code for second attempt of classify gender

---
+++ classify_gender_second_attempt.py
@@ -0,0 +1,22 @@
+import load_data
+import tune
+import label_gender
+
+val_conv, val_emb, val_rec, val_spk, val_text, val_mfcc = load_data.dataset()
+val_gender = label_gender.create_y_train(val_spk)
+
+def classify():
+    """
+    Method which calls for every layer the tune method in order to find the right parameters
+    :return:
+    """
+    file = './data/tuning/flickr8k-gender.txt'
+    tune.tune(val_mfcc, val_gender, file)
+    tune.tune(val_conv, val_gender, file)
+    amount_layers = val_rec.shape[1]
+    for i in range(amount_layers):
+        layer = val_rec[:, i, :]
+        tune.tune(layer, val_gender, file)
+    tune.tune(val_emb, val_gender, file)
+
+classify()
