72f5f503905cc5ee0e31eee15424ec94b6a4a8c0
markvdlaan93 <markkvdlaan93@gmail.com>
2018-06-28 13:11:10 +0200

    Count amount of speakers that only have a single instance

--- majority.py
+++ majority.py
@@ -7,6 +7,17 @@ def majority(val_spk):
     """
     unique, counts = np.unique(val_spk, return_counts=True)
     counts = np.transpose(np.asarray((unique, counts)))
+
+    frequencies = counts[:,1]
+
+    count_single_instances = 0
+    for frequency in frequencies:
+        if frequency == '1':
+            count_single_instances += 1
+
+    # 11 speakers that have only a single entry, stratification needs at least two instances of each speaker
+    #print(count_single_instances)
+
     # Problem was here. Labels were shifted!
     # counts = np.sort(counts, axis=0)
     counts = counts[counts[:,0].argsort()]
@@ -28,9 +39,17 @@ def majority_places(val_spk):
 
     counts = collections.Counter(val_spk)
 
+    count_single_instances = 0
+    for key, value in counts.items():
+        if value == 1:
+            count_single_instances += 1
+    # 40 speakers with only a single instance, stratification needs at least two instances of each speaker
+    #print(count_single_instances)
+
     # Majority baseline is 0.362
     print("Majority baseline is {}".format(max(counts.values()) / val_spk.shape[0]))
 
     return counts
 
-
+#majority(val_spk)
+#majority_places(val_spk_int)
