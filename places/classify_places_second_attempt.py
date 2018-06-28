bee5e52aa4a5e68834b34d279b19a9807a6e3775
Mark van der Laan <markkvdlaan93@gmail.com>
2018-06-28 02:22:17 +0200

    Second attempt tuning file for places dataset

---
+++ places/classify_places_second_attempt.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('../')
+import tune
+import load_data
+
+val_conv, val_emb, val_rec, _, val_spk_int, val_text, val_mfcc = load_data.dataset_places()
+
+def classify():
+    """
+    Method which calls for every layer the tune method in order to find the right parameters
+    :return:
+    """
+    file = './data/tuning/places-speaker.txt'
+    tune.tune(val_mfcc, val_spk_int, file)
+    tune.tune(val_conv, val_spk_int, file)
+    amount_layers = val_rec.shape[1]
+    for i in range(amount_layers):
+        layer = val_rec[:, i, :]
+        tune.tune(layer, val_spk_int, file)
+    tune.tune(val_emb, val_spk_int, file)
+
+classify()
