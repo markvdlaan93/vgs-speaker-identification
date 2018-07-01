c02a45ee1128bf1a904e23427488170b67331531
Mark van der Laan <markkvdlaan93@gmail.com>
2018-07-01 16:14:41 +0200

    Add image for gender speaker classification accuracy scores

--- places/explore_places.py
+++ places/explore_places.py
@@ -84,7 +84,25 @@ def gender_speaker_classification_scores():
     build_plot('../img/gender-speaker-classification.png', 0.6)
 
 def gender_speaker_accuracy_classification_scores():
-    return None
+    x = [1, 2, 3, 4, 5, 6, 7]
+    y = [0.8300, 0.8340, 0.9406, 0.8939, 0.8630, 0.8291, 0.6461]
+    my_xticks = ['MFCC', 'Conv.', 'Rec. 1', 'Rec. 2', 'Rec. 3', 'Rec. 4', 'Emb.']
+    plt.xticks(x, my_xticks)
+    plt.plot(x, y, label='Flickr8K speaker classification')
+
+    # Flickr8K gender classification F1-scores
+    y = [0.7491, 0.7521, 0.9552, 0.9564, 0.9418, 0.9339, 0.9055]
+    plt.plot(x, y, label='Flickr8K gender classification')
+
+    # Places gender classification F1-scores
+    y = [0.8650, 0.8750, 0.9750, 0.9600, 0.9675, 0.9575, 0.9250]
+    plt.plot(x, y, label='Places gender classification')
+
+    # Places speaker classification F1-score
+    y = [0.8200, 0.8300, 0.8850, 0.8350, 0.8375, 0.8325, 0.7825]
+    plt.plot(x, y, label='Places speaker classification')
+
+    build_plot('../img/gender-speaker-classification-accuracy.png', 0.6)
 
 def gender_bias_scores():
     """
@@ -112,6 +130,9 @@ def gender_bias_scores():
 
     build_plot('../img/gender-bias.png', 0.7)
 
+def gender_bias_accuracy_scores():
+    return None
+
 
 def build_plot(file_name, x_start):
     """
@@ -127,4 +148,4 @@ def build_plot(file_name, x_start):
     plt.savefig(file_name, bbox_extra_artists=(lgd,), bbox_inches='tight')
 
 
-gender_speaker_classification_scores()
+gender_speaker_accuracy_classification_scores()
