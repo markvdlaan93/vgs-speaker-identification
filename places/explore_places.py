3a2192e0520a09718901b9fd9ebb5ee2cc8e21f4
Mark van der Laan <markkvdlaan93@gmail.com>
2018-07-14 14:03:01 +0200

    Speaker identificaition for accuracy scores also dashed lines

--- places/explore_places.py
+++ places/explore_places.py
@@ -88,7 +88,7 @@ def gender_speaker_accuracy_classification_scores():
     y = [0.8300, 0.8340, 0.9406, 0.8939, 0.8630, 0.8291, 0.6461]
     my_xticks = ['MFCC', 'Conv.', 'Rec. 1', 'Rec. 2', 'Rec. 3', 'Rec. 4', 'Emb.']
     plt.xticks(x, my_xticks)
-    plt.plot(x, y, label='Flickr8K speaker classification')
+    plt.plot(x, y, label='Flickr8K speaker classification', linestyle='dashed')
 
     # Flickr8K gender classification F1-scores
     y = [0.7491, 0.7521, 0.9552, 0.9564, 0.9418, 0.9339, 0.9055]
@@ -100,7 +100,7 @@ def gender_speaker_accuracy_classification_scores():
 
     # Places speaker classification F1-score
     y = [0.8200, 0.8300, 0.8850, 0.8350, 0.8375, 0.8325, 0.7825]
-    plt.plot(x, y, label='Places speaker classification')
+    plt.plot(x, y, label='Places speaker classification', linestyle='dashed')
 
     build_plot('../img/gender-speaker-classification-accuracy.png', 0.6)
 
@@ -170,4 +170,4 @@ def build_plot(file_name, x_start):
 
 
 
-gender_speaker_classification_scores()
+gender_speaker_accuracy_classification_scores()
